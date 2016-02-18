c2faf1d6611889b8fc1be85926369e41fdf6d105
Mosab Sasi <smosab@gmail.com>
2016-02-18 07:28:49 -0700

    adding palindrome method

---
+++ palindrome.rb
@@ -0,0 +1,19 @@
+word = "tacocat"
+
+def palindrome?(word)
+    ## Convert the given parameter into String first
+    ## So that it can handle number too.
+    word = String(word);
+    x = 0;
+    y = word.length - 1;
+    while(x <= y)
+        if(word[x] != word[y])
+            return false;
+        end
+        x = x + 1;
+        y = y - 1;
+    end
+    return true;
+end
+
+palindrome?(word)
